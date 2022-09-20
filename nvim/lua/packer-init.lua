4657bfd8bf663e3a8ff6c7d77a6891adc5c113ea
Mark Westerhof <mwesterhof@fortinet.com>
2022-09-20 07:33:59 -0700

    Refactor lua config layout

--- nvim/lua/packer-init.lua
+++ nvim/lua/packer-init.lua
@@ -1,9 +1,23 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    print('Installing packer')
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 return require('packer').startup(function(use)
 
   use 'wbthomason/packer.nvim'
 
   -- Syntax highlighting/formatting
-  use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+  use {
+    'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
   use 'gpanders/editorconfig.nvim'
 
   -- Display plugins
@@ -40,5 +54,9 @@ return require('packer').startup(function(use)
   use 'L3MON4D3/LuaSnip'
   use 'saadparwaiz1/cmp_luasnip'
   use 'rafamadriz/friendly-snippets'
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 
 end)
---
+++ nvim/lua/plugins/mason.lua
@@ -0,0 +1,7 @@
+require('mason').setup()
+
+vim.api.nvim_create_user_command('MasonBootstrap', function(opts)
+  vim.cmd([[:MasonInstall typescript-language-server eslint-lsp css-lsp]])
+end, {
+    desc = "Bootstrap Mason by installing a few select packages."
+})
