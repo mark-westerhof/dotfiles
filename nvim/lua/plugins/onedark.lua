f87ca030a4d6473cf3feb99e2d7005c3841d9401
Mark Westerhof <mwesterhof@fortinet.com>
2022-10-01 10:36:24 -0700

    Customize some plugins highlights

--- nvim/lua/plugins/onedark.lua
+++ nvim/lua/plugins/onedark.lua
@@ -1,3 +1,15 @@
-require('onedark').load()
 
+require('onedark').setup {
+  highlights = {
+    TelescopeBorder = {fg = '$grey'},
+    TelescopePromptBorder = {fg = '$grey'},
+    TelescopeResultsBorder = {fg = '$grey'},
+    TelescopePreviewBorder = {fg = '$grey'},
+    DashboardHeader = {fg = '$light_grey'},
+    DashboardCenter = {fg = '$light_grey'},
+    DashboardShortCut = {fg = '$grey'}
+  }
+}
+
+require('onedark').load()
 vim.cmd('colorscheme onedark')
