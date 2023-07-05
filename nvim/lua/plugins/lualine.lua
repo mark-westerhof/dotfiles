0839d47a65825c797a6766164cc1fc514ec2aeef
Mark Westerhof <mwesterhof@fortinet.com>
2023-07-05 09:49:37 -0700

    Update lualine to show relative file path

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,11 +1,23 @@
 require('lualine').setup{
-   options = {
-     theme = 'onedark',
-     section_separators = '',
-     component_separators = '',
-     globalstatus = true,
-     disabled_filetypes = { 'packer', 'dashboard', 'NvimTree' }
-   }
+  options = {
+    theme = 'onedark',
+    section_separators = '',
+    component_separators = '',
+    globalstatus = true,
+    disabled_filetypes = { 'packer', 'fzf', 'NvimTree' }
+  },
+  sections = {
+    lualine_b = {
+      'diff',
+      'diagnostics'
+    },
+    lualine_c = {
+      {
+        'filename',
+        path = 1
+      }
+    }
+  }
 }
 
 vim.opt.cmdheight = 0
